051d7b23690932108e2f1125ec35d5a15289da4a
Arpitamo <arpitamondal5972@gmail.com>
2026-01-09 10:15:29 +0530

    Change the visibility of blog page

--- astro-hello-world/astro.config.mjs
+++ astro-hello-world/astro.config.mjs
@@ -1,21 +1,19 @@
-
-
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 import sanity from "@sanity/astro";
+
+import tunnel from "astro-tunnel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [
-    // 👇 update these lines
-    sanity({
-      projectId: "bql2xbpk",
-      dataset: "production",
-      useCdn: false, // for static builds
-    }),
-  ],
+  integrations: [// 👇 update these lines
+  sanity({
+    projectId: "bql2xbpk",
+    dataset: "production",
+    useCdn: false, // for static builds
+  }), tunnel()],
 });
